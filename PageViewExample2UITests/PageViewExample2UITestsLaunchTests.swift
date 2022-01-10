5680f73822a65521dbea0ea0961e408699aa885d
Maximiliano Riquelme Vera <maximiliano.riquelme@globant.com>
2022-01-10 12:19:11 -0300

    Initial Commit

---
+++ PageViewExample2UITests/PageViewExample2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PageViewExample2UITestsLaunchTests.swift
+//  PageViewExample2UITests
+//
+//  Created by Maximiliano Riquelme Vera on 10/01/2022.
+//
+
+import XCTest
+
+class PageViewExample2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
